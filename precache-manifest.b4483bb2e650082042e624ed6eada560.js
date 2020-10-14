bc021defd444a04d927e705c5be760ae7df2d9fe
ahmedashin999 <ahmedashin999@gmail.com>
2020-10-14 20:50:33 +0600

    Updates

---
+++ precache-manifest.b4483bb2e650082042e624ed6eada560.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecbce0c74788a0f763be95e26f3df7c6",
+    "url": "/quiz/index.html"
+  },
+  {
+    "revision": "a5896ab7602742cdc196",
+    "url": "/quiz/static/css/main.3df262a9.chunk.css"
+  },
+  {
+    "revision": "1d010e9c989a36247aec",
+    "url": "/quiz/static/js/2.65522de4.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/quiz/static/js/2.65522de4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5896ab7602742cdc196",
+    "url": "/quiz/static/js/main.8fe4ece9.chunk.js"
+  },
+  {
+    "revision": "a9479cdaad8d2ca8d55c",
+    "url": "/quiz/static/js/runtime-main.1f7be3d1.js"
+  },
+  {
+    "revision": "8bf87f41e74ff98d1c2ad1fbdb0a1b89",
+    "url": "/quiz/static/media/image.8bf87f41.jpg"
+  }
+]);
